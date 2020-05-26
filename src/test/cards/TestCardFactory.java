b587235211325006ff0acde7820d978e767228b7
Karl <karllamoureux2@gmail.com>
2020-05-26 14:44:44 -0400

    Wrote test which tests main

--- src/test/cards/TestCardFactory.java
+++ src/test/cards/TestCardFactory.java
@@ -1,5 +1,25 @@
 package test.cards;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import main.cards.Card_Factory;
+import main.players.Player_Factory;
+
 public class TestCardFactory {
+	
+	private static Card_Factory factory;
+	private static final String TEST_CARD_NAME = "Test";
+	private static final String CARD_DIRECTORY = "card_folder";
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		factory = new Card_Factory();
+	}
+	
+	@Test
+	void testCreate() {
+		factory.createCard();
+	}
 
 }
